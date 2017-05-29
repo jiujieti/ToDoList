99ea5213c8c09a7bb84c29227f7064189e2560a3
jiujieti <melody2014ymq@gmail.com>
2017-05-29 20:27:16 +0200

    add connection to mongoDB

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # To Do List
 A web application - to do list
+
+1. View engine: pug
+2. Database: mongoDB provided by mLab
--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var users = require('./routes/users');
 
 var app = express();
 
+// set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://<dbuser>:<dppassword>@ds155811.mlab.com:55811/user_todolists';
+mongoose.connect(mongoDB);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
