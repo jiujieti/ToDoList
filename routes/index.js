942a139e5f71103e00770e60fab5c5a8bd692cfd
jiujieti <melody2014ymq@gmail.com>
2017-05-30 22:08:38 +0200

    add a post request triggered by form element

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'To Do List - 未竟的事业' });
+});
+
+/* POST user, redirect to a user's to do list page */
+router.post('/todolist', (req, res) => {
+  res.render('todolist', { title: 'Your To Do List - 您未竟的事业' });
 });
 
 module.exports = router;
