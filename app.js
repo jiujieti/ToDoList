af43069ffd75e8f37c71228b94180438851b320e
jiujieti <melody2014ymq@gmail.com>
2017-06-08 19:36:24 +0200

    delete unused users.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var list = require('./routes/list');
 
 var app = express();
@@ -31,7 +30,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/list', list);
 
 // catch 404 and forward to error handler
