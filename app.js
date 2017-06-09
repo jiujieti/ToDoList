f2ce5ce0ca8b635e53655c8a4d7610959299138f
jiujieti <melody2014ymq@gmail.com>
2017-06-09 19:11:24 +0200

    rename the files

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var list = require('./routes/list');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/list', list);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.redirect('/list');
+  res.redirect('/login');
 });
 
 module.exports = router;
