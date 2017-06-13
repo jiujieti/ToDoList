c0e437e139715d6934d6f33933e5dd35c5e26586
jiujieti <melody2014ymq@gmail.com>
2017-06-13 23:07:02 +0200

    add hash and salt fields

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,12 @@
 var User = require('../models/user');
 
 exports.create_user = (req, res) => {
-  if(req.body.password == req.body.pdconfirm) {
-    var newUser = new User({
+  var newUser = new User({
       username: req.body.username,
-      password: req.body.password
-    });
+      hash: req.body.password,
+      salt: req.body.pdconfirm
+  });
+  if(req.body.password == req.body.pdconfirm) {
     newUser.save((error) => {
       if(error) { throw error; }
       res.redirect('/login');
--- models/user.js
+++ models/user.js
@@ -3,8 +3,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = Schema({
-  username: {type: String, required: true, max: 30},
-  password: {type: String, required: true, max: 30, min: 8}
+  username: {type: String, required: true, max: 30, unique: true},
+  hash: {type: String, required: true},
+  salt: {type: String, required: true}
 });
 
 module.exports = mongoose.model('User', UserSchema);
