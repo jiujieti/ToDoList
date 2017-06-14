6182b9829099ad4554b64691bdc2a4d730ecc838
jiujieti <melody2014ymq@gmail.com>
2017-06-14 23:03:40 +0200

    Add empty lines

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,11 @@ var bcrypt = require('bcrypt');
 exports.create_user = (req, res) => {
   
   if(req.body.password === req.body.pdconfirm) {
+    
     if(req.body.password.length <= 8) {
       res.render('Signup', {'error': 'Password cannot be less than 8 characters.'});
     }
+    
     var genSalt = bcrypt.genSalt(10);
     var genHash = bcrypt.hash(req.body.password, genSalt);
     var newUser = new User({
@@ -14,11 +16,13 @@ exports.create_user = (req, res) => {
       hash: genHash,
       salt: genSalt
     });
+  
     newUser.save((error) => {
       if(error) {
         res.render('Signup', {'error': error.message});
       }
     });
+  
   } else {
     res.render('Signup', {'error': 'Passwords are different! Please enter them again.'});
   }
