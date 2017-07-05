fbcc22b721e6b83d8608eb07d470d006c0cf65a3
jiujieti <melody2014ymq@gmail.com>
2017-07-05 21:17:30 +0200

    add simple test

---
+++ test.js
@@ -0,0 +1,33 @@
+// set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://mengqi:12345678@ds155811.mlab.com:55811/user_todolists';
+mongoose.connect(mongoDB);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var Schema = mongoose.Schema;
+var UserSchema = Schema({
+  username: { type: String, required: true, max: 30, unique: true },
+  password: { type: String, required: true }
+});
+
+var User = mongoose.model('User', UserSchema);
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  user.password = user.password + "hello";
+  next();
+});
+
+ var newUser = new User({
+    username: "jiujieti",
+    password: "12345678"
+});
+  
+newUser.save((error) => {
+  if(error) {
+    console.log("Hi");
+    return;
+  }
+  console.log("yes!");
+});
